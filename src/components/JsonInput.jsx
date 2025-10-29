90c6b9299b832743017c8eeafc1b9f2c8ea22c83
myfirst-demo <suhailullkhan619@gmail.com>
2025-10-29 16:56:44 +0530

    final

--- src/components/JsonInput.jsx
+++ src/components/JsonInput.jsx
@@ -15,11 +15,11 @@ export default function JsonInput({ jsonText, setJsonText, onVisualize, error,da
       {error && <p className="error-text">{error}</p>}
       <button onClick={onVisualize} className="generate-btn">Generate Tree</button>
       
-      <div style={{display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'flex-start',padding:10,gap:'30px'}}>
-      <div style={{display:'flex',flexDirection:'row',gap:'20px'}}><span style={{width:'80px',height:'20px',background:"#e0e7ff",border:"2px solid #6366f1",borderRadius:'6px'}}></span> is for Object</div>
-      <div style={{display:'flex',flexDirection:'row',gap:'20px'}}><span style={{width:'80px',height:'20px',background:"#dcfce7",border:"2px solid #16a34a",borderRadius:'6px'}}></span> is for Array</div>
-      <div style={{display:'flex',flexDirection:'row',gap:'20px'}}><span style={{width:'80px',height:'20px',background:"#fef3c7",border:"2px solid #f59e0b",borderRadius:'6px'}}></span> is for Array values</div>
-      <div style={{display:'flex',flexDirection:'row',gap:'20px'}}><span style={{width:'80px',height:'20px',background:"#f1f5f9",border:"1px solid #94a3b8",borderRadius:'6px'}}></span> is for Primitive types</div>
+      <div style={{display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'left',padding:10,gap:'30px'}}>
+      <div style={{display:'flex',flexDirection:'row',gap:'20px',width:'100%'}}><span style={{width:'80px',height:'20px',background:"#e0e7ff",border:"2px solid #6366f1",borderRadius:'6px'}}></span> is for Object</div>
+      <div style={{display:'flex',flexDirection:'row',gap:'20px',width:'100%'}}><span style={{width:'80px',height:'20px',background:"#dcfce7",border:"2px solid #16a34a",borderRadius:'6px'}}></span> is for Array</div>
+      <div style={{display:'flex',flexDirection:'row',gap:'20px',width:'100%'}}><span style={{width:'80px',height:'20px',background:"#fef3c7",border:"2px solid #f59e0b",borderRadius:'6px'}}></span> is for Array values</div>
+      <div style={{display:'flex',flexDirection:'row',gap:'20px',width:'100%'}}><span style={{width:'80px',height:'20px',background:"#f1f5f9",border:"1px solid #94a3b8",borderRadius:'6px'}}></span> is for Primitive types</div>
       <div>To access array use index with [ ] ex:user.skills[0]</div>
 
       </div>
